ee7ac1e72cff8b3c9af28a579f557e248d41360b
IvelinPenchev <ivelin.penchev.95@gmail.com>
2018-05-23 01:08:38 +0200

    Final Debug
    Debugging successful. Expanded filters settings to 8. Created folder for results.

--- BackgroundSubtraction/BackgroundSubtraction/BackgroundSubtraction.h
+++ BackgroundSubtraction/BackgroundSubtraction/BackgroundSubtraction.h
@@ -1,4 +1,15 @@
 #pragma once
+#include "BackgroundSubtraction.h"
+#include <iostream>
+#include "opencv2\highgui\highgui.hpp"
+#include "opencv\cv.h"
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <cmath>
+#include <array>
+#include <opencv/cv.h>
+using namespace cv;
+using namespace std;
 
 class BackgroundSubtractor {
 public:
@@ -14,9 +25,10 @@ private:
 	Mat edges_;
 
 	int iteration;
-	array<int, 6> median_{ 101, 101, 101, 201, 201, 201 };
-	array<int, 6> brightness_{ 100, 50, -150, 100, 50, -150 };
-	array<double, 6> contrast_{ 1, 1.8, 1.8, 1, 1.8, 1.8 };
+	array<int, 8> brightness_{ -80, -80, 100, 50, -150, -170, 50, -150};
+	array<double, 8> contrast_{ 1.6, 1.6, 1.6, 1.8, 1.8, 1.4, 1.8, 1.8 };
+	array<int, 8> median_{ 101, 175, 101, 101, 101, 175, 175, 175 };
+
 	int cannyLow_ = 5;
 	int cannyHigh_ = 70;
 
--- BackgroundSubtraction/BackgroundSubtraction/SubtractBackground.cpp
+++ BackgroundSubtraction/BackgroundSubtraction/SubtractBackground.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include "opencv2\highgui\highgui.hpp"
 #include "opencv\cv.h"
 #include "opencv2/core/core.hpp"
@@ -6,7 +7,7 @@
 using namespace cv;
 
 int main(int argc, char const *argv[]){
-	const cv::String destination = "Small blade//2.JPG";
+	const cv::String destination = "Small blade//6.JPG";
 	BackgroundSubtractor subtractor;
 	subtractor.background_subtraction(destination);
 }
